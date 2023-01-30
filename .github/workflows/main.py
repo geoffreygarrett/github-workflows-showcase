62231b581dd23894f03fb8e0b63e7076dd783fa7
Geoffrey Garrett <g.h.garrett13@gmail.com>
2023-01-30 22:13:04 +0200

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json 
+import subprocess
 
 # Get environment variables
 github_token = os.environ['GH_TOKEN']
@@ -8,6 +9,32 @@ github_workspace = os.environ['GITHUB_WORKSPACE']
 github_repository = os.environ['GITHUB_REPOSITORY']
 github_event_path = os.environ['GITHUB_EVENT_PATH']
 github_event_name = os.environ['GITHUB_EVENT_NAME']
+
+def run_command(command):
+    # Function to run shell commands
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout, stderr
+
+def git_flow_init():
+    # Initialize GitFlow on repository
+    run_command(f"git clone https://x-access-token:{github_token}@github.com/{github_repository}.git {github_workspace}")
+    os.chdir(github_workspace)
+    run_command("git flow init -d")
+
+def start_feature(feature_name):
+    # Start a new feature in GitFlow
+    run_command(f"git flow feature start {feature_name}")
+    run_command("git push --set-upstream origin feature/{feature_name}")
+
+def finish_feature(feature_name):
+    # Finish a feature in GitFlow
+    run_command(f"git flow feature finish {feature_name}")
+    run_command("git push")
+
+def delete_feature(feature_name):
+    # Delete a feature in GitFlow
+    run_command(f"git flow feature delete {feature_name}")
 
 def main():
   # Handle repository dispatch events
@@ -18,14 +45,17 @@ def main():
 
     # Handle actions based on the payload
     if payload['action'] == 'start_feature':
-      # Start feature action logic here
-      pass
+      feature_name = payload['feature_name']
+      git_flow_init()
+      start_feature(feature_name)
     elif payload['action'] == 'finish_feature':
-      # Finish feature action logic here
-      pass
+      feature_name = payload['feature_name']
+      git_flow_init()
+      finish_feature(feature_name)
     elif payload['action'] == 'delete_feature':
-      # Delete feature action logic here
-      pass
+      feature_name = payload['feature_name']
+      git_flow_init()
+      delete_feature(feature_name)
 
 if __name__ == "__main__":
   main()
